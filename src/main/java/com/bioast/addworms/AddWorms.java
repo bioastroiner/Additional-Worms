1ea1d6811ed2778266ac30c02daca3aa5e1be2fe
Bioastroiner <naderi81eco@gmail.com>
2020-12-28 04:57:37 +0330

    moved the itemGroup to and made them static

--- src/main/java/com/bioast/addworms/AddWorms.java
+++ src/main/java/com/bioast/addworms/AddWorms.java
@@ -1,32 +1,18 @@
 package com.bioast.addworms;
 
-import com.bioast.addworms.client.*;
-import com.bioast.addworms.init.InitRegister;
+import com.bioast.addworms.init.ModBlocks;
 import com.bioast.addworms.init.ModItems;
-import net.minecraft.block.Block;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.item.IItemPropertyGetter;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.world.World;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import javax.annotation.Nullable;
-
 import static com.bioast.addworms.AddWorms.*;
-import static com.bioast.addworms.init.ModEntityTypes.*;
 
 @Mod(MODID)
 @Mod.EventBusSubscriber(modid = MODID,bus = Mod.EventBusSubscriber.Bus.MOD)
@@ -37,20 +23,25 @@ public class AddWorms
     public static AddWorms instance;
 
     public AddWorms() {
-        final IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-        bus.addListener(this::setup);
-        InitRegister.ENTITY_TYPES.register(bus);
-        InitRegister.ITEMS.register(bus);
-        InitRegister.BLOCKS.register(bus);
-
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         instance = this;
         MinecraftForge.EVENT_BUS.register(this);
     }
 
     private void setup(final FMLCommonSetupEvent event) {
+    }
 
-    }
-    @SubscribeEvent
-    public void onServerStarting(FMLServerStartingEvent event) {
-    }
+    public static final ItemGroup WormsBlockGroup = new ItemGroup(ItemGroup.GROUPS.length,
+            new TranslationTextComponent("tab." + MODID + "blocks").getFormattedText()) {
+        @Override
+        public ItemStack createIcon() {
+            return new ItemStack(ModBlocks.MUDY_DIRT.get());
+        }
+    };
+    public static final ItemGroup WormsItemGroup = new ItemGroup(ItemGroup.GROUPS.length, new TranslationTextComponent("tab." + MODID + "items").getFormattedText()) {
+        @Override
+        public ItemStack createIcon() {
+            return new ItemStack(ModItems.WORM.get());
+        }
+    };
 }
