cb4546f035ccd6bef738cafd2355f2c16bf5b6ac
Bioastroiner <naderi81eco@gmail.com>
2021-03-02 04:39:18 +0330

    basic implementation of miner upgrade

--- src/main/java/com/bioast/addworms/entities/worm/MinerWormEntity.java
+++ src/main/java/com/bioast/addworms/entities/worm/MinerWormEntity.java
@@ -1,11 +1,11 @@
 package com.bioast.addworms.entities.worm;
 
 import com.bioast.addworms.items.worms.GeneralWormItem;
-import com.bioast.addworms.utils.helpers.ParticleHelper;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
-import net.minecraft.particles.ParticleTypes;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
@@ -25,7 +25,28 @@ public class MinerWormEntity extends AbstractWormEntity {
 
     @Override
     public ActionResultType applyPlayerInteraction(PlayerEntity player, Vector3d vec, Hand hand) {
-        return super.applyPlayerInteraction(player, vec, hand);
+        boolean inr = false;
+        if (player.getHeldItem(hand).getItem() == Items.GLOWSTONE) {
+            inr = true;
+        }
+        if (player.getHeldItem(hand).getItem() == Items.REDSTONE) {
+            inr = true;
+        }
+        if (player.getHeldItem(hand).getItem() == Items.OBSIDIAN) {
+            addItems(new ItemStack(Items.OBSIDIAN));
+            inr = true;
+        }
+        if (player.getHeldItem(hand).getItem() == Items.PISTON) {
+            addItems(new ItemStack(Items.PISTON));
+            inr = true;
+        }
+        if (inr) {
+            if (!player.isCreative())
+                player.getHeldItem(hand).shrink(1);
+            return ActionResultType.SUCCESS;
+        } else {
+            return ActionResultType.PASS;
+        }
     }
 
     @Override
@@ -37,8 +58,9 @@ public class MinerWormEntity extends AbstractWormEntity {
     }
 
     private int getMiningRate() {
-        int rate = 20 - (int) getSpeed();
+        int rate = 10 - (int) getSpeed();
         if (doseVoid && rate > 1) --rate;
+        if (rate < 1) rate = 1;
         return rate;
     }
 
@@ -47,9 +69,14 @@ public class MinerWormEntity extends AbstractWormEntity {
     }
 
     private int getVerRange() {
-        return -5;
+        ItemStack piss = new ItemStack(Items.PISTON, 0);
+        simpleItemStorage.forEach(i -> {
+            if (i.getItem() == Items.PISTON) {
+                piss.grow(i.getCount());
+            }
+        });
+        return -piss.getCount();
         // applying pistone increaces heaigh
-        // applying st-pistone decreaces heigh
         // applying obsidian will make it void worm
         // they all still start at the worm's itself heigh (change)
     }
@@ -58,16 +85,16 @@ public class MinerWormEntity extends AbstractWormEntity {
         int size = getMiningRange();
         BlockPos.getAllInBox(
                 new BlockPos(getPosition().getX() - size, getPosition().getY(), getPosition().getZ() - size),
-                new BlockPos(getPosition().getX() + size, getPosition().getY(), getPosition().getZ() + size))
+                new BlockPos(getPosition().getX() + size, getPosition().getY() - getVerRange(), getPosition().getZ() + size))
                 .filter(b -> !world.isAirBlock(b))
                 .filter(b -> world.getBlockState(b).isSolid())
                 .filter(b -> world.getBlockState(b).getBlockHardness(world, b) > 0)
                 .filter(b -> !getPosition().equals(b.toImmutable()))
                 .findAny()
                 .ifPresent(b -> currentPos = b.toImmutable());
+        //ParticleHelper.spawnParticles(world, getPosition(), 15, ParticleTypes.DUST);
         if (!world.isRemote) {
             world.destroyBlock(currentPos, !doseVoid);
-            ParticleHelper.spawnParticles(world, getPosition(), 15, ParticleTypes.DUST);
         }
     }
 }
